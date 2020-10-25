269acf56b1d96d244cad05586b06e97c4312647c
Keith Williams <thestreetkw@hotmail.com>
2020-10-25 17:57:47 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,10 +1,17 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
+  cart.each do |i|
+  if i[:item] == coupons[:item] && 
+    i[:count] >= coupons[:number]
+    cart.push({item: "AVOCADO W/COUPON", price: 2.50, clearance: true, count:2})
+    end 
+  end
+end 
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
